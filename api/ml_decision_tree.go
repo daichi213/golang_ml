d22404d0c1a523a541707f9dcfc39da4c1c82106
尾崎大地 <ozakidaichi@ozakidaichinoMacBook-Pro.local>
2021-10-31 15:55:59 +0900

    update

--- api/ml_decision_tree.go
+++ api/ml_decision_tree.go
@@ -5,8 +5,30 @@ import (
 	"log"
 	"strconv"
 	"net/http"
+	"math/rand"
 	"github.com/gin-gonic/gin"
 )
 
 // TODO Decision Tree Algorithmを実装する(できればRandomForestかBoostingを実装する)
 
+type CaliforniaHousingData struct {
+	MedInc float64
+	HouseAge float64
+	AveRooms float64
+	AveBedrms float64
+	Population float64
+	AveOccup float64
+	Latitude float64
+	Longitude float64
+}
+
+// type options func(*DecisionOptions)
+
+// func DecisionOptions(v string) options {
+// 	return func(o *DecisionOptions)
+// }
+
+func decisionTree(data []*CaliforniaHousingData, features_num int) {
+
+	chosenFeature := rand.Int31n(features_num)
+}
